d4ebd08953ea5046a9ab444d8f827de8f75c8102
Abhinav Singh <singhabhinav2205@gmail.com>
2026-05-30 17:35:07 +0530

    backend fix

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,53 +1,49 @@
 import axios from "axios";
-import fs from "fs"
+import fs from "fs";
 import FormData from "form-data";
 import userModel from "../models/userModel.js";
-
-//controller function to remove bg
+import connectDB from "../configs/mongodb.js";
 
 const removeBgImage = async (req, res) => {
     try {
+        await connectDB();
 
-        const {clerkId} = req.body;
+        const clerkId = req.clerkId;
 
-        const user = await userModel.findOne({ clerkId })
+        const user = await userModel.findOne({ clerkId });
 
         if (!user) {
             return res.json({ success: false, message: "User not found" });
         }
-        
+
         if (user.creditBalance === 0) {
-            return res.json({ success: false, message: "Insufficient credits" ,creditBalance:user.creditBalance});
+            return res.json({ success: false, message: "Insufficient credits", creditBalance: user.creditBalance });
         }
+
         const imagePath = req.file.path;
 
-        //reading image file
-        const imageFile = fs.createReadStream(imagePath)
+        const imageFile = fs.createReadStream(imagePath);
 
-        //form data
         const formData = new FormData();
         formData.append("image_file", imageFile);
 
-        const {data} = await axios.post("https://clipdrop-api.co/remove-background/v1", formData, {
+        const { data } = await axios.post("https://clipdrop-api.co/remove-background/v1", formData, {
             headers: {
                 "x-api-key": process.env.CLIPDROP_API
             },
             responseType: "arraybuffer"
-        })
+        });
 
         const base64Image = Buffer.from(data, "binary").toString("base64");
-
         const resultImage = `data:${req.file.mimetype};base64,${base64Image}`;
 
-       await userModel.findOneAndUpdate(user._id,{creditBalance:user.creditBalance - 1})
+        await userModel.findOneAndUpdate({ _id: user._id }, { creditBalance: user.creditBalance - 1 });
 
-       res.json({ success: true, resultImage, creditBalance:user.creditBalance - 1 ,message:"Background removed successfully"})
+        res.json({ success: true, resultImage, creditBalance: user.creditBalance - 1, message: "Background removed successfully" });
 
-        
     } catch (error) {
-         console.log("Error:", error.message);
+        console.log("Error:", error.message);
         res.json({ success: false, message: error.message });
-        
     }
 }
 
